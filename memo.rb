5ee09c68f5842c1353ae545a5faa04ca7652dcc2
bore-ko <tori.un.sourire@gmail.com>
2023-06-27 22:15:35 +0900

    DB接続の処理をメソッド化

--- memo.rb
+++ memo.rb
@@ -12,18 +12,20 @@ helpers do
     ERB::Util.html_escape(text)
   end
 
+  def connect_memos
+    PG.connect(dbname: 'memos')
+  end
+
   def memo_lines
     id = params[:id].delete(':')
-    conn = PG.connect(dbname: 'memos')
-    @memos = conn.exec('SELECT * FROM memos WHERE id = $1;', [id])
-    conn.finish
+    @memos = connect_memos.exec('SELECT * FROM memos WHERE id = $1;', [id])
+    connect_memos.finish
   end
 end
 
 get '/' do
-  conn = PG.connect(dbname: 'memos')
-  @memos = conn.exec('SELECT * FROM memos;')
-  conn.finish
+  @memos = connect_memos.exec('SELECT * FROM memos;')
+  connect_memos.finish
   erb :index
 end
 
@@ -34,9 +36,8 @@ end
 post '/memos' do
   title = params[:title]
   content = params[:content]
-  conn = PG.connect(dbname: 'memos')
-  conn.exec('INSERT INTO memos(title, content) VALUES ($1, $2);', [title, content])
-  conn.finish
+  connect_memos.exec('INSERT INTO memos(title, content) VALUES ($1, $2);', [title, content])
+  connect_memos.finish
   redirect '/'
 end
 
@@ -54,16 +55,14 @@ patch '/memos/:id' do
   id = params[:id].delete(':')
   title = params[:title]
   content = params[:content]
-  conn = PG.connect(dbname: 'memos')
-  conn.exec('UPDATE memos SET title=$1, content=$2 WHERE id=$3;', [title, content, id])
-  conn.finish
+  connect_memos.exec('UPDATE memos SET title=$1, content=$2 WHERE id=$3;', [title, content, id])
+  connect_memos.finish
   redirect '/'
 end
 
 delete '/memos/:id' do
   id = params[:id].delete(':')
-  conn = PG.connect(dbname: 'memos')
-  conn.exec('DELETE FROM memos WHERE id=$1;', [id])
-  conn.finish
+  connect_memos.exec('DELETE FROM memos WHERE id=$1;', [id])
+  connect_memos.finish
   redirect '/'
 end
